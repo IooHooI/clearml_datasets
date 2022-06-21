348c61e78a92bf1cbf17361c486a807f574d9e16
Efim Golovin <golovin.e@selectel.ru>
2022-06-21 15:10:13 +0300

    Add some code and explanations

---
+++ cifar10/dataset_1_creation.py
@@ -0,0 +1,11 @@
+from clearml import Dataset
+
+
+if __name__ == "__main__":
+    cifar10_dataset = Dataset.create(
+        dataset_project='clearml_datasets',
+        dataset_name='CIFAR10'
+    )
+
+    for dataset in Dataset.list_datasets(dataset_project='clearml_datasets', only_completed=False):
+        print(dataset)
---
+++ mnist/dataset_2_meta_info_preparation.py
@@ -0,0 +1,17 @@
+from clearml import Dataset
+
+if __name__ == "__main__":
+    mnist_dataset = Dataset.get(
+        dataset_project='clearml_datasets',
+        dataset_name='MNIST'
+    )
+
+    mnist_dataset.add_tags([
+        'image',
+        'classification',
+        'example',
+        'small'
+    ])
+
+    for dataset in Dataset.list_datasets(dataset_project='clearml_datasets', only_completed=False):
+        print(dataset)
