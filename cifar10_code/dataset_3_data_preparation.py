bd8e8fea870492ae96093ab37d8e9f3390ca58c1
Efim Golovin <golovin.e@selectel.ru>
2022-06-22 14:18:19 +0300

    mnist dataset

--- cifar10_code/dataset_3_data_preparation.py
+++ cifar10_code/dataset_3_data_preparation.py
@@ -1,22 +1,11 @@
-import os
 from clearml import Dataset
 
 
 if __name__ == "__main__":
-    cifar10_dataset = Dataset.get(
-        dataset_project='clearml_datasets',
-        dataset_name='CIFAR10'
-    )
+    cifar10_dataset = Dataset.get(dataset_project='clearml_datasets', dataset_name='CIFAR10')
 
-    cifar10_dataset.add_files(
-        path='../tmp/cifar10/train',
-        dataset_path='data/train',
-        verbose=True
-    )
+    cifar10_dataset.add_files(path='../tmp/cifar10/train', dataset_path='data/train', verbose=True)
 
-    Dataset.upload(
-        cifar10_dataset,
-        verbose=True
-    )
+    Dataset.upload(cifar10_dataset, verbose=True)
 
     cifar10_dataset.finalize()
