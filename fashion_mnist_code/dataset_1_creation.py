67fe3ba064e3a71024fc0cc9f78e167ac60acb99
Efim Golovin <golovin.e@selectel.ru>
2022-06-22 14:49:28 +0300

    small fixes

--- fashion_mnist_code/dataset_1_creation.py
+++ fashion_mnist_code/dataset_1_creation.py
@@ -4,5 +4,5 @@ from clearml import Dataset
 if __name__ == "__main__":
     fashion_mnist_dataset = Dataset.create(dataset_project='clearml_demo/clearml_datasets', dataset_name='FASHION_MNIST')
 
-    for dataset in Dataset.list_datasets(dataset_project='clearml_datasets', only_completed=False):
+    for dataset in Dataset.list_datasets(dataset_project='clearml_demo/clearml_datasets', only_completed=False):
         print(dataset)
