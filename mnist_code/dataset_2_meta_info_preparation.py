6f3143a9cd356d5b7395f47eac267146dac7c63a
Efim Golovin <golovin.e@selectel.ru>
2022-06-22 14:26:09 +0300

    small fixes

--- mnist_code/dataset_2_meta_info_preparation.py
+++ mnist_code/dataset_2_meta_info_preparation.py
@@ -1,17 +1,9 @@
 from clearml import Dataset
 
 if __name__ == "__main__":
-    mnist_dataset = Dataset.get(
-        dataset_project='clearml_datasets',
-        dataset_name='MNIST'
-    )
+    mnist_dataset = Dataset.get(dataset_project='clearml_demo/clearml_datasets', dataset_name='MNIST')
 
-    mnist_dataset.add_tags([
-        'image',
-        'classification',
-        'example',
-        'small'
-    ])
+    mnist_dataset.add_tags(['image', 'classification', 'example', 'small'])
 
     for dataset in Dataset.list_datasets(dataset_project='clearml_datasets', only_completed=False):
         print(dataset)
