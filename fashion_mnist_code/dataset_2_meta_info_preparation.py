b2b7bb895e51a8d81ab5e37dcbfe5cc4f1707e7e
Efim Golovin <golovin.e@selectel.ru>
2022-06-22 14:26:09 +0300

    small fixes

--- fashion_mnist_code/dataset_2_meta_info_preparation.py
+++ fashion_mnist_code/dataset_2_meta_info_preparation.py
@@ -1,17 +1,9 @@
 from clearml import Dataset
 
 if __name__ == "__main__":
-    fashion_mnist_dataset = Dataset.get(
-        dataset_project='clearml_datasets',
-        dataset_name='FASHION_MNIST'
-    )
+    fashion_mnist_dataset = Dataset.get(dataset_project='clearml_demo/clearml_datasets', dataset_name='FASHION_MNIST')
 
-    fashion_mnist_dataset.add_tags([
-        'image',
-        'classification',
-        'example',
-        'small'
-    ])
+    fashion_mnist_dataset.add_tags(['image', 'classification', 'example', 'small'])
 
     for dataset in Dataset.list_datasets(dataset_project='clearml_datasets', only_completed=False):
         print(dataset)
--- mnist_code/dataset_1_creation.py
+++ mnist_code/dataset_1_creation.py
@@ -2,10 +2,7 @@ from clearml import Dataset
 
 
 if __name__ == "__main__":
-    mnist_dataset = Dataset.create(
-        dataset_project='clearml_datasets',
-        dataset_name='MNIST'
-    )
+    mnist_dataset = Dataset.create(dataset_project='clearml_demo/clearml_datasets', dataset_name='MNIST')
 
     for dataset in Dataset.list_datasets(dataset_project='clearml_datasets', only_completed=False):
         print(dataset)
