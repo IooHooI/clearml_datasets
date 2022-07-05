84ac40c06f657c4994cc48958430c5747bee79dd
Efim Golovin <golovin.e@selectel.ru>
2022-07-05 11:55:18 +0300

    small fixes

--- source/cifar10_code/dataset_4_external_data_preparation.py
+++ source/cifar10_code/dataset_4_external_data_preparation.py
@@ -1,29 +1,41 @@
 import os
 
-from clearml import Dataset
-
 from clearml.utilities.pyhocon import ConfigFactory
 
-from source.auxiliary_code import global_config
+import boto3
+
+from clearml import Dataset
+
+# from source.auxiliary_code import global_config
 
 
 if __name__ == "__main__":
-    dataset_name = 'CIFAR10'
+    # dataset_name = 'CIFAR10'
+    #
+    # cifar10_dataset = Dataset.get(dataset_project=global_config.DATASET_PROJECT, dataset_name=dataset_name)
+    #
+    # data_path = os.path.join(global_config.get_temp_data_path(), dataset_name)
 
-    cifar10_dataset = Dataset.get(dataset_project=global_config.DATASET_PROJECT, dataset_name=dataset_name)
+    config = ConfigFactory.parse_file(os.path.join(os.environ["HOME"], "clearml.conf"))
 
-    data_path = os.path.join(global_config.get_temp_data_path(), dataset_name)
+    print(config)
 
-    clearml_conf = ConfigFactory.parse_file(os.path.join(os.environ["HOME"], "clearml.conf"))
+    # client = boto3.client(
+    #     service_name="s3",
+    #     region_name=clearml_conf["sdk"]["aws"]["s3"]["region"],
+    #     endpoint_url="https://{}".format(clearml_conf["sdk"]["aws"]["s3"]["host"]),
+    #     aws_access_key_id=clearml_conf["sdk"]["aws"]["s3"]["key"],
+    #     aws_secret_access_key=clearml_conf["sdk"]["aws"]["s3"]["secret"]
+    # )
+    #
+    # object_list = client.list_objects(Bucket=clearml_conf["sdk"]["aws"]["s3"]["credentials"][0]["bucket"])
 
-    cifar10_dataset.add_external_files(
-        source_url="s3://{}/{}/{}".format(
-            clearml_conf["sdk"]["aws"]["s3"]["host"],
-            clearml_conf["sdk"]["aws"]["s3"]["credentials"][0]["bucket"],
-            "clearml_demo/clearml_datasets/EXTERNAL_FILES"
-        ),
-        dataset_path=os.path.join(dataset_name, 'train/external'),
-        verbose=True
-    )
-
-    Dataset.upload(cifar10_dataset, verbose=True)
+    # cifar10_dataset.add_files(
+    #     path=os.path.join(data_path, 'train'),
+    #     dataset_path=os.path.join(dataset_name, 'train'),
+    #     verbose=True
+    # )
+    #
+    # Dataset.upload(cifar10_dataset, verbose=True)
+    #
+    # cifar10_dataset.finalize()
--- source/cifar10_code/dataset_5_get_local_dataset_copy.py
+++ source/cifar10_code/dataset_5_get_local_dataset_copy.py
@@ -4,9 +4,7 @@ from source.auxiliary_code import global_config
 
 
 if __name__ == "__main__":
-    dataset_name = 'CIFAR10'
-
-    cifar10_dataset = Dataset.get(dataset_project=global_config.DATASET_PROJECT, dataset_name=dataset_name)
+    cifar10_dataset = Dataset.get(dataset_project=global_config.DATASET_PROJECT, dataset_name='CIFAR10')
 
     target_folder = cifar10_dataset.get_local_copy()
 
